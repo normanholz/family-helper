4da9291a599b56c3c315debbc81ec6faf227e4c3
Norman Holz <normanholz@gmail.com>
2021-03-08 21:40:14 +0100

    added form and reducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 import Amplify, {API, graphqlOperation} from 'aws-amplify';
 import awsConfig from './aws-exports';
 import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
@@ -7,13 +7,33 @@ import { listLists } from './graphql/queries'
 import MainHeader from './components/headers/MainHeader';
 import 'semantic-ui-css/semantic.min.css';
 import Lists from './components/List/Lists';
-import { Container, Button, Icon, Modal } from 'semantic-ui-react'
+import { Container, Button, Icon, Modal, Form } from 'semantic-ui-react'
 
 Amplify.configure(awsConfig);
 
+const initialState = {
+  title: '',
+  description: ''
+}
+
+function listReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'DESCRIPTION_CHANGED':
+      return {...state, description: action.value}
+    case 'TITLE_CHANGED':
+      return {...state, title: action.value}
+    default:
+      console.log('Default action for', action)
+      return state;
+  }
+}
+
+
 function App() {
+
+  const [state, dispatch] = useReducer(listReducer, initialState)
   const [lists, setLists] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(true);
   async function fetchList() {
     const {data} = await API.graphql(graphqlOperation(listLists));
     setLists(data.listLists.items);
@@ -42,7 +62,18 @@ function App() {
         <Modal.Header>
           Create your list
         </Modal.Header>
-        <Modal.Content>Form to be added later</Modal.Content>
+        <Modal.Content>
+          <Form>
+            <Form.Input             
+            error={true ? false : {content: 'Please add a title to your list'}}
+            label='Title' placeholder='My pretty List' value={state.title}
+            onChange={(e)=> dispatch({type: 'TITLE_CHANGED', value: e.target.value})}            
+            />            
+            <Form.TextArea label='Description' placeholder='Things that my pretty list is about' value={state.description}            
+            onChange={(e)=> dispatch({type: 'DESCRIPTION_CHANGED', value: e.target.value})}      
+            ></Form.TextArea>
+          </Form>
+        </Modal.Content>
         <Modal.Actions>
           <Button negative onClick={() => toggleModal(false)}>Cancel</Button>
           <Button positive onClick={() => toggleModal(false)}>Save</Button>
